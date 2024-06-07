92b5bf9d730d58b611c7cd38ec5e9345e6abd8f7
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-06-07 02:17:20 +0000

    add tiny shakespeare and create an example little batch out of it

--- train_gpt2.py
+++ train_gpt2.py
@@ -166,18 +166,32 @@ if torch.cuda.is_available():
 elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
+device = "cpu" # OVERRIDE
 
-num_return_sequences = 5
-max_length = 30
-
-# model = GPT.from_pretrained('gpt2')
-model = GPT(GPTConfig())
-model.eval()
-model.to(device)
-
-# prefix tokens
+# get a data batch
 import tiktoken
 enc = tiktoken.get_encoding('gpt2')
+with open('input.txt', 'r') as f:
+    text = f.read()
+text = text[:1000]
+tokens = enc.encode(text)
+B, T = 4, 32
+buf = torch.tensor(tokens[:B*T + 1])
+x = buf[:-1].view(B, T)
+y = buf[1:].view(B, T)
+
+# get logits
+model = GPT(GPTConfig())
+model.to(device)
+logits = model(x)
+
+print(logits.shape)
+import sys; sys.exit(0)
+
+# prefix tokens
+model.eval()
+num_return_sequences = 5
+max_length = 30
 tokens = enc.encode("Hello, I'm a language model,")
 tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
