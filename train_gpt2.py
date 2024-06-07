7230096129a52ae763cd810f2bb9e61f67ec9ab7
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-06-07 23:46:48 +0000

    vocab size 50257 -> 50304 nice

--- train_gpt2.py
+++ train_gpt2.py
@@ -228,7 +228,7 @@ train_loader = DataLoaderLite(B=16, T=1024)
 torch.set_float32_matmul_precision('high')
 
 # get logits
-model = GPT(GPTConfig())
+model = GPT(GPTConfig(vocab_size=50304))
 model.to(device)
 model = torch.compile(model)
 
