7822fce8ebf46ba3907ee8f7cf0356f8ec1c63fa
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-06-07 02:53:52 +0000

    little loop crushes a little batch

--- train_gpt2.py
+++ train_gpt2.py
@@ -169,7 +169,6 @@ if torch.cuda.is_available():
 elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
-device = "cpu" # OVERRIDE
 
 # get a data batch
 import tiktoken
@@ -180,15 +179,23 @@ text = text[:1000]
 tokens = enc.encode(text)
 B, T = 4, 32
 buf = torch.tensor(tokens[:B*T + 1])
+buf = buf.to(device)
 x = buf[:-1].view(B, T)
 y = buf[1:].view(B, T)
 
 # get logits
 model = GPT(GPTConfig())
 model.to(device)
-logits, loss = model(x, y)
 
-print(loss)
+# optimize!
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
+
 import sys; sys.exit(0)
 
 # prefix tokens
