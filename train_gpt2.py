ec39245425b2c82a38c0787f27e5f9d7082c973b
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-06-07 17:28:44 +0000

    weight tie the embedding and unembedding matrix

--- train_gpt2.py
+++ train_gpt2.py
@@ -92,6 +92,9 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
         B, T = idx.size()
