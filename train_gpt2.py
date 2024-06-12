12c1ea851d36b69f8ad249713d0c03b228df2442
Andrej Karpathy <andrej.karpathy@gmail.com>
2024-06-12 14:51:58 +0000

    convert uint16 to int32 before cast to long, otherwise pytorch doesn't like it

--- train_gpt2.py
+++ train_gpt2.py
@@ -207,6 +207,7 @@ import numpy as np
 
 def load_tokens(filename):
     npt = np.load(filename)
+    npt = npt.astype(np.int32) # added after video
     ptt = torch.tensor(npt, dtype=torch.long)
     return ptt
 
