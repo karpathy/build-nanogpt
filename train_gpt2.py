046e046348a504d9e741f321647b7cc8285bcf60
Srinivas Billa <nivibilla@gmail.com>
2024-06-13 16:43:51 +0100

    Update train_gpt2.py

--- train_gpt2.py
+++ train_gpt2.py
@@ -393,7 +393,7 @@ for step in range(max_steps):
                 torch.save(checkpoint, checkpoint_path)
 
     # once in a while generate from the model (except step 0, which is noise)
-    if ((step > 0 and step % 25 == 0) or last_step) and (not use_compile):
+    if ((step >= 0 and step % 25 == 0) or last_step) and (not use_compile):
         model.eval()
         num_return_sequences = 4
         max_length = 32
